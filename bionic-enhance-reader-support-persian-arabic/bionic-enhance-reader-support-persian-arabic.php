d0bfac5b3e23290418b14315c42f454f5d4ecf95
Ali Soleimani <30648015+alisol-dev@users.noreply.github.com>
2022-06-10 17:38:07 +0430

    Add files via upload

---
+++ bionic-enhance-reader-support-persian-arabic/bionic-enhance-reader-support-persian-arabic.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * bionic-enhance-reader-support-persian-arabic plugin
+ *
+ * Activate this plugin then:
+ * Every h1,h2,h3,h4,h5,h6,p tags will be converted to bionic text automatically.
+ * Enjoy it!
+ *
+ * @link              https://github.com/alisol-dev
+ * @since             1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Bionic Enhance Reader (Support Persian Arabic)
+ * Plugin URI:        https://github.com/alisol-dev/Bionic-Enhance-Reader-Support-Persian-Arabic-WP-Plugin/
+ * Description:       WordPress plugin to make bionic all posts on your website to enhance reading speed and quality.
+ * Version:           1.0.0
+ * Author:            Alisol
+ * Author URI:        https://github.com/alisol-dev
+ * License:           GPLv3
+ * License URI:       http://www.gnu.org/licenses/gpl.html
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+// TODO for next version: 
+    // add option for users to choose using bionicText.js with .bionicText class 
+    // or using bionicText.all.js for applying on all paragraphs.
+
+
+function load_css_js() {
+    // wp_enqueue_style( 'gdgt-base', get_template_directory_uri() . '/css/gdgt-base.css', false, NULL, 'all' );
+    // wp_enqueue_style( 'gdgt-icon', get_template_directory_uri() . '/icons/css/gdgt.css', false, NULL, 'all' );
+
+    wp_register_script( 'bionicText', '/wp-content/plugins/bionic-enhance-reader-support-persian-arabic/js/bionicText.all.js' , array(), false, true );
+    wp_enqueue_script( 'bionicText' );
+}
+
+add_action( 'wp_enqueue_scripts', 'load_css_js' );
---
+++ bionic-enhance-reader-support-persian-arabic/js/bionicText.all.js
@@ -0,0 +1,97 @@
+function ModifyTextBasic(textNodeContent) {
+    return textNodeContent.split(' ').map((word) => {
+
+        var boldUp2 = Math.floor(0.5 * Math.floor(word.length / 2));
+        let firstHalf = word.substring(0, boldUp2 + 1);
+        let secondHalf = word.substring(boldUp2 + 1);
+
+        let firstHalfSuffix = "";
+        let secondHalfPreffix = "";
+        let lastCharOfArabicWord = getLastCharOfArabicWord(firstHalf);
+        let firstCharOfArabicWord = getFirstCharOfArabicWord(secondHalf);
+        if (doesArabicCharNeedSuffixJoiner(lastCharOfArabicWord) === true && doesArabicCharNeedPreffixJoiner(firstCharOfArabicWord) === true) {
+            firstHalfSuffix = "\u200D";
+            secondHalfPreffix = "\u200D";
+        }
+
+        return word.replace(word, `<b>${firstHalf}${firstHalfSuffix}</b>${secondHalfPreffix}${secondHalf}`);
+    });
+}
+
+function getLastCharOfArabicWord(word) {
+    let len = word.length;
+    let lastChar = word.charAt(len - 1);
+    return lastChar;
+}
+
+function getFirstCharOfArabicWord(word) {
+    let firstChar = word.charAt(0);
+    return firstChar;
+}
+
+function doesArabicCharNeedSuffixJoiner(char) {
+    let arabicCharsNeedingSuffixJoiner = ["ء",
+        "أ",
+        "ب",
+        "پ",
+        "ت",
+        "ث",
+        "ج",
+        "چ",
+        "ح",
+        "خ",
+        "س",
+        "ش",
+        "ص",
+        "ض",
+        "ط",
+        "ظ",
+        "ع",
+        "غ",
+        "ف",
+        "ق",
+        "ک",
+        "گ",
+        "ل",
+        "م",
+        "ن",
+        "ه",
+        "ی"
+    ];
+
+    if (arabicCharsNeedingSuffixJoiner.includes(char)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function doesArabicCharNeedPreffixJoiner(char) {
+    // all arabic chars needs preffix joiner.
+    var arabic = /[\u0600-\u06FF]/;
+    if (arabic.test(char)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function ModifyWebPage() {
+    const domParser = new DOMParser();
+    // var allText = [...document.querySelectorAll('.bionicText > *:is(h1,h2,h3,h4,h5,h6,p), .bionicText:is(h1,h2,h3,h4,h5,h6,p)')]; // for custom using in HTML pages
+    var allText = [... document.querySelectorAll('h1,h2,h3,h4,h5,h6,p')]; // for browser extension
+    allText.forEach(element => {
+        var text = domParser.parseFromString(element.innerHTML, "text/html");
+        var textNodeCollection = Array.from(text.body.childNodes).map((node) => {
+            if (node.nodeType === Node.TEXT_NODE)
+                return ModifyTextBasic(node.textContent).join(' ');
+            else
+                return node.outerHTML;
+        });
+        element.innerHTML = textNodeCollection.join('');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    ModifyWebPage();
+}, false);
